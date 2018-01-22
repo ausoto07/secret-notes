e955e2b18f4c1ad2520a5ca89caa6360b4178cb8
Alex <ausoto07@gmail.com>
2018-01-22 15:24:22 -0500

    logged in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,9 +12,7 @@ class UsersController < ApplicationController
     private
   def user_params
     params.
-    require(:user).
-    permit(:username,
-    :password,
-    :password_confirmation)
+      require(:user).
+      permit(:username, :password,:password_confirmation)
   end
 end
