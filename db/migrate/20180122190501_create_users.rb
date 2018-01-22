7bc1a57dab912fef15ddcff2813fe2cdbfc45dad
Alex <ausoto07@gmail.com>
2018-01-22 14:13:45 -0500

    spike out signup form

--- db/migrate/20180122190501_create_users.rb
+++ db/migrate/20180122190501_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.stirng :username
-      t.sting :password_digest
+      t.string :username
+      t.string :password_digest
 
       t.timestamps
     end
